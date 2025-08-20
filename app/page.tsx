131e19c58dcda63f440742631d50a5c444023530
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-20 14:41:58 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import AddNewRecord from '@/components/AddNewRecord';
-import ExpenseStats from '@/components/ExpenseStats';
 import Guest from '@/components/Guest';
-import RecordChart from '@/components/RecordChart';
-import RecordHistory from '@/components/RecordHistory';
 import { currentUser } from '@clerk/nextjs/server';
 
 export default async function HomePage() {
